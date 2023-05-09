a04342c483129bdc4e330949383abddba5f530b1
shumakooov <shumakov.13@yandex.ru>
2023-05-09 17:20:06 +0500

    booking card and device card fix

--- src/app/components/modal-windows/modal-booking-card/modal-booking-card.component.ts
+++ src/app/components/modal-windows/modal-booking-card/modal-booking-card.component.ts
@@ -1,15 +1,17 @@
 import {ChangeDetectionStrategy, Component, Inject, Injector, OnInit} from '@angular/core';
 import {FormControl, FormGroup, Validators} from '@angular/forms';
-import {TUI_DATE_FORMAT, TuiDay, TuiTime, TuiTimeMode} from "@taiga-ui/cdk";
-import {tuiCreateTimePeriods, tuiInputTimeOptionsProvider} from "@taiga-ui/kit";
-import {TuiAlertService, TuiDialogContext, TuiDialogService} from "@taiga-ui/core";
+import {TUI_DATE_FORMAT, tuiControlValue, TuiDay, TuiMonth, TuiTime, TuiTimeMode, TuiYear} from "@taiga-ui/cdk";
+import {TUI_CALENDAR_DATE_STREAM, tuiCreateTimePeriods, tuiInputTimeOptionsProvider} from "@taiga-ui/kit";
+import {TUI_MONTHS, TuiAlertService, TuiDialogContext, TuiDialogService} from "@taiga-ui/core";
 import {POLYMORPHEUS_CONTEXT, PolymorpheusComponent} from "@tinkoff/ng-polymorpheus";
-import {Observable} from "rxjs";
+import {Observable, withLatestFrom} from "rxjs";
 import {Device, Record} from "../../../interfaces";
 import {DeviceService} from "../../../services/device.service";
 import {Router} from "@angular/router";
 import {environment} from "../../../../environments/environment";
 import {ModalCalendarComponent} from "../modal-calendar/modal-calendar.component";
+import {TuiMobileCalendarDialogComponent} from "@taiga-ui/addon-mobile";
+import {map, startWith} from "rxjs/operators";
 
 @Component({
   selector: 'app-modal-booking-card',
@@ -30,7 +32,31 @@ export class ModalBookingCardComponent implements OnInit{
               private deviceService: DeviceService,
               private router: Router,
               @Inject(TuiAlertService)
-              private readonly alertService: TuiAlertService) { }
+              private readonly alertService: TuiAlertService,
+              @Inject(TUI_MONTHS) private readonly months: Observable<string[]>,) {
+    const dataStream = tuiControlValue(this.controlCal);
+    const computedInjector = Injector.create({
+      providers: [
+        {
+          provide: TUI_CALENDAR_DATE_STREAM,
+          useValue: dataStream,
+        },
+      ],
+      parent: injector,
+    });
+    const content = new PolymorpheusComponent(
+      TuiMobileCalendarDialogComponent,
+      computedInjector,
+    );
+
+    this.dialog$ = dialogService.open(content, {
+      size: 'fullscreen',
+      closeable: false,
+      data: {
+        min: TuiDay.currentLocal(),
+      },
+    });
+  }
 
   device$: Observable<Device>
   timeStartForm: FormGroup
@@ -70,13 +96,39 @@ export class ModalBookingCardComponent implements OnInit{
 
   items1 = tuiCreateTimePeriods();
 
-  valueCalendar: string | null = null;
-  valueCalendarTuiDay: TuiDay | null = null;
 
-  onDayClick(day: TuiDay): void {
-    this.valueCalendar = [day.year, day.formattedMonthPart, day.formattedDayPart].join('-');
-    this.valueCalendarTuiDay = day;
+//calendar
+  valueCalendar: string | null = null;
+  private readonly controlCal = new FormControl(TuiDay.fromLocalNativeDate(new Date()));
+  private readonly dialog$: Observable<TuiDay>;
+  readonly date$ = this.controlCal.valueChanges.pipe(
+    startWith(this.controlCal.value),
+    withLatestFrom(this.months),
+    map(([value, months]) => this.getParsed(value, months)),
+  );
+
+  get empty(): boolean {
+    return !this.controlCal.value;
   }
+
+  getParsed(value: TuiDay | null, months: string[]): string {
+    if (!value) {
+      return 'Choose a date';
+    }
+
+    const {month, day, year} = value;
+
+    this.valueCalendar = [value.year, value.formattedMonthPart, value.formattedDayPart].join('-');
+    return `${months[month]} ${day}, ${year}`;
+  }
+
+  onClick(): void {
+    this.dialog$.subscribe(value => {
+      this.controlCal.setValue(value);
+    });
+  }
+//
+
 
   recordDevice(){
     const record = {
